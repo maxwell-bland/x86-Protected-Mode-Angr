1fa9d233a0ed0f28433ccb265287be1d948a3243
maxwell-bland <mb28@illinois.edu>
2022-04-01 15:33:58 -0500

    Works.

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # x86-protected-mode-angr
-x86 protected mode support for angr
+
+x86 protected mode support for angr.
+
+```
+pip3 install angr
+pip3 install iced_x86
+```
+
+### Features left to implement.
+
+- Segment translation currently indexes into the GDT/LDT directly, thus this 
+  does not support code that actively modifies the GDT/LDT, pretty uncommon.
+- No interrupt support
+- No task switching/callgate support
+
+## 
+
+Example use:
+```
+from angr_x86_protected_support import *
+
+entry_points = {
+        'set_two_fields': 0x1080cb9,
+        'parse_ops': 0x108ba40,
+        'while_loops': 0x1094d80
+}
+loader = cle.Loader('app.bin',
+                    main_opts={'backend': 'blob',
+                               'arch': 'i386',
+                               'base_addr': 0x1000000,
+                               'entry_point': 0x300cb9
+                    },
+                    rebase_granularity=0x1000,
+                    page_size=1,
+                    auto_load_libs=False)
+
+f = '00100000_00205fff.bin'
+b = cle.backends.Blob(f, open(f, 'rb'), loader=loader,
+                      is_main_bin=False, arch='i386', auto_load_libs=False,
+                      base_addr=0x100000, pic=True)
+loader.dynamic_load(b)
+
+f = '00300120_0031a618.bin'
+b = cle.backends.Blob(f, open(f, 'rb'), loader=loader, is_main_bin=False,
+                      arch='i386', auto_load_libs=False, base_addr=0x300120, pic=True)
+loader.dynamic_load(b)
+
+dt = '00003964_00003d7b.bin'
+b = cle.backends.Blob(dt, open(dt, 'rb'), loader=loader, is_main_bin=False, arch='i386',
+                      auto_load_libs=False, base_addr=0x3964, pic=True)
+loader.dynamic_load(b)
+
+dt = '00003fa4_000040c3.bin'
+b = cle.backends.Blob(dt, open(dt, 'rb'), loader=loader, is_main_bin=False, arch='i386',
+                      auto_load_libs=False, base_addr=0x3fa4, pic=True)
+loader.dynamic_load(b)
+
+p = angr.Project(loader)
+state = p.factory.entry_state()
+state.regs.cs = 0x00000007
+state.regs.eip = 0xb99
+state.regs.eax = 0x00104F0C
+state.regs.ebx = 0x00104D0C
+state.regs.ds = 0x0000000F
+state.regs.ecx = 0x000000F8
+state.regs.edx = 0x00104E0C
+state.regs.edi = 0x00031630
+state.regs.flags = 0x00000246
+state.regs.esi = 0x00104E0C
+state.regs.ebp = 0x51050EAD
+state.regs.esp = 0x00104D0C
+state.regs.ss = 0x0000000F
+state.regs.es = 0x00000037
+state.regs.fs = 0x00000047
+state.regs.gs = 0x00000047
+state.regs.gdt = state.solver.Concat(claripy.BVV(
+        0x00003964, 32), claripy.BVV(0x00000417, 32)).zero_extend(16)
+state.regs.ldt = state.solver.Concat(claripy.BVV(
+    0x00003fa4, 32), claripy.BVV(0x0000011f, 32)).zero_extend(16)
+
+state.memory.store(0x00204e0c,claripy.BVS('buffer',9*8))
+
+AngrX86().x86init(state)
+
+sm = p.factory.simulation_manager(state)
+
+sm.step(successor_func=x86step)
+```
---
+++ angr_x86_protected_support.py
@@ -0,0 +1,110 @@
+import angr
+import cle
+import claripy
+import IPython
+from iced_x86 import *
+from types import ModuleType
+from typing import Dict, Sequence
+
+def dt_lookup(state, sel, offset, base=None):
+    # Protected mode.
+    if base is None:
+        if sel & 4:
+            # LDT
+            base = state.solver.eval(state.regs.ldt[:32])
+            limit = state.solver.eval(state.regs.ldt[32:])
+        else:
+            # GDT
+            base = state.solver.eval(state.regs.gdt[:32])
+            limit = state.solver.eval(state.regs.gdt[32:].concrete)
+
+    sel = sel & 0xf8
+    if sel + 7 > limit:
+        raise Exception('Attempted to index GDT/LDT with segment greater than limit')
+    table_addr = (base + sel)
+    descriptor = state.solver.eval(
+                   state.memory.load(
+                     table_addr, 8, disable_actions=True, inspect=False), cast_to=bytes
+                 )
+    seg_base = ((descriptor[2] | (descriptor[3] << 8) | 
+                (descriptor[4] << 16) | (descriptor[7] << 24)))
+    return seg_base + offset
+
+def x86step(state, **kwargs):
+    eip_base = dt_lookup(
+    state, 
+                         state.solver.eval(
+                             state.regs.cs
+                         ), 
+                         0)
+    state.regs.eip += eip_base
+    successors = state.project.factory.successors(state, **kwargs)
+    state.regs.eip -= eip_base
+    for s in successors:
+        s.regs.eip -= eip_base
+    return successors
+
+class AngrX86():
+    def __init__(self):
+        self.info_factory = InstructionInfoFactory()
+
+        def create_enum_dict(module: ModuleType) -> Dict[int, str]:
+            return {module.__dict__[key]:key for key in module.__dict__ if isinstance(module.__dict__[key], int)}
+
+        self.reg_map = create_enum_dict(Register)
+        self.opac_map = create_enum_dict(OpAccess)
+
+    def used_mem_to_string(self, mem_info):
+        sb = "[" + self.reg_map[mem_info.segment] + ":"
+        need_plus = mem_info.base != Register.NONE
+        if need_plus:
+            sb += self.reg_map[mem_info.base]
+        if mem_info.index != Register.NONE:
+            if need_plus:
+                sb += "+"
+            need_plus = True
+            sb += self.reg_map[mem_info.index]
+            if mem_info.scale != 1:
+                sb += "*" + str(mem_info.scale)
+        if mem_info.displacement != 0 or not need_plus:
+            if need_plus:
+                sb += "+"
+            sb += f"0x{mem_info.displacement:X}"
+        sb += ";" + self.opac_map[mem_info.access] + "]"
+        return sb
+
+    def get_accessed_seg(self, i, t):
+        '''
+        @param i is the iced_x86 instruction
+        @param t is the the type of access to look for (read or write)
+        '''
+        for mi in self.info_factory.info(i).used_memory():
+            ot = 'write' if 'WRITE' in self.opac_map[mi.access] else 'read'
+            if ot == t:
+                return self.reg_map[mi.segment].lower()
+    
+    def x86_translate(self, state, t='read'):
+        state.block().pp()
+        seg = self.get_accessed_seg(
+            [i for i in Decoder(32, state.block().disassembly.insns[0].bytes)][0], t
+        )
+
+        # Weird mismatched read to write problem in angr
+        if not seg:
+            return
+
+        seg = state.solver.eval(getattr(state.regs, seg))
+
+        if t == 'read':
+            state.inspect.mem_read_address = dt_lookup(state, seg, 
+                state.solver.eval(state.inspect.mem_read_address))
+        else:
+            state.inspect.mem_write_address = dt_lookup(state, seg, 
+                state.solver.eval(state.inspect.mem_write_address))
+        
+
+    def x86init(self, state):
+        state.inspect.b('mem_write', when=angr.BP_BEFORE, action=lambda s: self.x86_translate(s,'write'))
+        state.inspect.b('mem_read', when=angr.BP_BEFORE, action=lambda s: self.x86_translate(s))
+        state.inspect.b('mem_write', when=angr.BP_AFTER, action=lambda s: self.x86_translate(s,'write'))
+        state.inspect.b('mem_read', when=angr.BP_AFTER, action=lambda s: self.x86_translate(s))

06b44cdf331219e595eb633d02c29d94b664d929
maxwell-bland <mb28@illinois.edu>
2022-04-04 16:47:00 -0500

    Add support for LES instructions.

--- angr_x86_protected_support.py
+++ angr_x86_protected_support.py
@@ -2,9 +2,40 @@ import angr
 import cle
 import claripy
 import IPython
+import traceback
 from iced_x86 import *
 from types import ModuleType
 from typing import Dict, Sequence
+
+# iced_x86 resolvers
+
+def create_enum_dict(module: ModuleType) -> Dict[int, str]:
+    return {module.__dict__[key]:key for key in module.__dict__ if isinstance(module.__dict__[key], int)}
+
+MNEMONIC_MAP = create_enum_dict(Mnemonic)
+REG_MAP = create_enum_dict(Register)
+OPAC_MAP = create_enum_dict(OpAccess)
+INFO_FACT = InstructionInfoFactory()
+
+def get_mem_info(mem_info):
+    seg = REG_MAP[mem_info.segment].lower()
+    base_reg = None
+    scale = 1
+    displacement = 0
+    index_reg = None
+    if mem_info.base != Register.NONE:
+        base_reg = REG_MAP[mem_info.base].lower()
+    if mem_info.index != Register.NONE:
+        index_reg = REG_MAP[mem_info.index].lower()
+        scale = mem_info.scale
+    if mem_info.displacement:
+        displacement = mem_info.displacement
+    access_type = OPAC_MAP[mem_info.access]
+    return {'seg': seg, 'base_reg': base_reg, 'scale': scale,
+            'displacement': displacement, 'index_reg': index_reg,
+            'access_type': access_type}
+
+# GDT and LDT lookups
 
 def dt_lookup(state, sel, offset, base=None):
     # Protected mode.
@@ -12,15 +43,15 @@ def dt_lookup(state, sel, offset, base=None):
         if sel & 4:
             # LDT
             base = state.solver.eval(state.regs.ldt[:32])
-            limit = state.solver.eval(state.regs.ldt[32:])
+            limit = state.solver.eval(state.regs.ldt[31:])
         else:
             # GDT
             base = state.solver.eval(state.regs.gdt[:32])
-            limit = state.solver.eval(state.regs.gdt[32:].concrete)
+            limit = state.solver.eval(state.regs.gdt[31:])
 
     sel = sel & 0xf8
     if sel + 7 > limit:
-        raise Exception('Attempted to index GDT/LDT with segment greater than limit')
+        raise Exception(f'Attempted to index GDT/LDT with segment {sel} greater than limit {limit}')
     table_addr = (base + sel)
     descriptor = state.solver.eval(
                    state.memory.load(
@@ -30,6 +61,46 @@ def dt_lookup(state, sel, offset, base=None):
                 (descriptor[4] << 16) | (descriptor[7] << 24)))
     return seg_base + offset
 
+# Instruction handlers
+
+def les_fix(state, inst_len, write_reg='eax', seg='ds', base_reg=None, scale=1, displacement=0, index_reg=None, **kwargs):
+    addr = 0
+    if base_reg:
+        addr = state.solver.eval(getattr(state.regs,base_reg))
+    if index_reg:
+        addr += state.solver.eval(getattr(state.regs,base_reg)) * scale
+    if displacement:
+        addr += displacement
+
+    mem_val = state.memory.load(
+        dt_lookup(state, state.solver.eval(getattr(state.regs, seg)), addr), 
+        6, 
+        disable_actions=True, 
+        inspect=False
+    ).reversed
+
+    setattr(state.regs,write_reg,mem_val[31:])
+    state.regs.es = mem_val[:32]
+    state.regs.pc += inst_len
+
+def x86_instruction_fixes(state):
+    i = [i for i in Decoder(
+        32, state.solver.eval(
+            state.memory.load(
+                state.addr, 
+                20, 
+                disable_actions=True, 
+                inspect=False
+            ), cast_to=bytes))][0]
+    if MNEMONIC_MAP.get(i.mnemonic) == 'LES':
+        info = INFO_FACT.info(i)
+        mi = info.used_memory()[0]
+        mi = get_mem_info(mi)
+        mi['write_reg'] = REG_MAP[info.used_registers()[-2].register].lower()
+        les_fix(state, i.len, **mi)
+
+# Custom simulation steppers
+
 def x86step(state, **kwargs):
     if state.history.jump_balance < 0:
         eip_base = dt_lookup(state, state.solver.eval(state.regs.cs), 0)
@@ -37,6 +108,11 @@ def x86step(state, **kwargs):
         state.history.jump_balance = 0
 
     successors = state.project.factory.successors(state, **kwargs)
+
+    for s in successors:
+        if s.history.jumpkind == 'Ijk_NoDecode' and not s.project.is_hooked(s.history.jump_target):
+            state.project.hook(s.solver.eval(s.history.jump_target), hook=x86_instruction_fixes)
+            return x86step(state)
 
     for s in successors:
         if s.history.jumpkind == 'Ijk_Ret':
@@ -48,44 +124,21 @@ def x86step(state, **kwargs):
 
     return successors
 
+# Angr breakpoint handlers and initialization
+
 class AngrX86():
     def __init__(self):
-        self.info_factory = InstructionInfoFactory()
-
-        def create_enum_dict(module: ModuleType) -> Dict[int, str]:
-            return {module.__dict__[key]:key for key in module.__dict__ if isinstance(module.__dict__[key], int)}
-
-        self.reg_map = create_enum_dict(Register)
-        self.opac_map = create_enum_dict(OpAccess)
-
-    def used_mem_to_string(self, mem_info):
-        sb = "[" + self.reg_map[mem_info.segment] + ":"
-        need_plus = mem_info.base != Register.NONE
-        if need_plus:
-            sb += self.reg_map[mem_info.base]
-        if mem_info.index != Register.NONE:
-            if need_plus:
-                sb += "+"
-            need_plus = True
-            sb += self.reg_map[mem_info.index]
-            if mem_info.scale != 1:
-                sb += "*" + str(mem_info.scale)
-        if mem_info.displacement != 0 or not need_plus:
-            if need_plus:
-                sb += "+"
-            sb += f"0x{mem_info.displacement:X}"
-        sb += ";" + self.opac_map[mem_info.access] + "]"
-        return sb
+        pass
 
     def get_accessed_seg(self, i, t):
         '''
         @param i is the iced_x86 instruction
         @param t is the the type of access to look for (read or write)
         '''
-        for mi in self.info_factory.info(i).used_memory():
-            ot = 'write' if 'WRITE' in self.opac_map[mi.access] else 'read'
+        for mi in INFO_FACT.info(i).used_memory():
+            ot = 'write' if 'WRITE' in OPAC_MAP[mi.access] else 'read'
             if ot == t:
-                return self.reg_map[mi.segment].lower()
+                return REG_MAP[mi.segment].lower()
     
     def x86_translate(self, state, t='read'):
         seg = self.get_accessed_seg(
@@ -96,14 +149,16 @@ class AngrX86():
         if not seg:
             return
 
-        seg = state.solver.eval(getattr(state.regs, seg))
+        seg_val = state.solver.eval(getattr(state.regs, seg))
 
-        if t == 'read':
-            state.inspect.mem_read_address = dt_lookup(state, seg, 
-                state.solver.eval(state.inspect.mem_read_address))
-        else:
-            state.inspect.mem_write_address = dt_lookup(state, seg, 
-                state.solver.eval(state.inspect.mem_write_address))
+        try:
+            if t == 'read':
+                state.inspect.mem_read_address += dt_lookup(state, seg_val, 0)
+            else:
+                state.inspect.mem_write_address += dt_lookup(state, seg_val, 0)
+        except Exception as e:
+            print(traceback.format_exc())
+            IPython.embed()
         
 
     def x86init(self, state):
